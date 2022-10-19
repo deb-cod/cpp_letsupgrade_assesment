44f06ce496fb85760b896280ff45e3fe7ea38328
deb-cod <83533796+deb-cod@users.noreply.github.com>
2022-10-19 20:19:40 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the other integer:";
+    cin>>b;
+    if(a==b)
+    {
+        cout<<"Integers are equal";
+    }
+    else
+    {
+        cout<<"Integers are not equal";
+    }
+    return 0;
+}
